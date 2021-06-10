3ccdb191886e4b3f4f3fe86dbc2e57bf5f00eca1
arzubekm <arzubekm@gmail.com>
2021-06-10 16:25:31 +0900

    Working on projects grid part

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -6,28 +6,11 @@ const Navbar = () => {
     return (
         <div>
             <div className="container">
-                <div className={styles.navbar}>
-                    <nav class="nav">
-                        <div class="col-3">
-                            <a href="/" class="nav-link active" aria-current="page"><h3>arzubekm</h3></a>
-                        </div>
-                        <div class="col-4"></div>
-                        <div class="col-1">
-                            <Link href="/"><a class="nav-link"><h5>Home</h5></a></Link>    
-                        </div>
-                        <div class="col-1">
-                            <Link href="/projects"><a class="nav-link"><h5>Projects</h5></a></Link>
-                        </div>
-                        <div class="col-1">
-                            <a class="nav-link"><h5>Git</h5></a>
-                        </div>
-                        <div class="col-1">
-                            <a class="nav-link"><h5>LinkedIn</h5></a>
-                        </div>
-                        <div class="col-1">
-                            <a class="nav-link"><h5>Instagram</h5></a>
-                        </div>
-                    </nav>
+                <div className={styles.navigation}>
+                    <a href="/">About</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#contact">Contact</a>
                 </div>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
       <div className="container">
-        <div>
-          <div class="row-2">
-            <h1>Objective</h1>
-            <p>A page built with <a href="https://nextjs.org/" target="_blank">Next.js</a>.</p>
-          </div>
-          <div class="row-1">
-            <div class="col-3">
-              <img src="avatar.jpg" class={styles.imgavatar} alt="Arzu"/>
-              <h4>Arzubek Murtazaev</h4>
-              <h7>Ulsan, South Korea</h7>
-            </div>
-            <div class="col-9">
-              <h1>Info</h1>
-            </div>
+        <div id="about" className={styles.identity}>
+          <img src="avatar.jpg" class={styles.imgavatar} width={100} height={100} alt="Arzu"/>
+          <div className={styles.text}>
+            <div className={styles.name}>Arzubek Murtazaev</div>
+            <div className={styles.description}>Data Scientist, Web Developer</div>
+            <div className={styles.description}>Ulsan, South Korea</div>
           </div>
         </div>
+        <div id="skills" className={styles.skills}>
+          <div className={styles.title}>Skills</div>
+          <div className={styles.row}>
+            <div className={styles.left}>Data Science</div>
+            <div className={styles.right}>Machine/Deep Learning, Jupyter, Python</div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.left}>Programming Languages</div>
+            <div className={styles.right}>Python, Java, Javascript</div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.left}>Frontend</div>
+            <div className={styles.right}>React.js, Next.js, Html, CSS, jQuery</div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.left}>Backend</div>
+            <div className={styles.right}>Django, Node.js</div>
+          </div>
+        </div>
+        <div id="projects" className={styles.projects}>
+          <div className={styles.title}>Projects</div>
+        </div>
+        <div className={styles.contact}></div>
       </div>
     </div>
   )
